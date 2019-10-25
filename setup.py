c9ebec4f3f1ede0d409a6d2a134ad6c89134b088
Zach Pace <zpace21@gmail.com>
2019-10-25 08:58:45 -0500

    remove all by py3.7 because dataclasses

--- setup.py
+++ setup.py
@@ -45,8 +45,6 @@ def run(packages, install_requires):
               'Natural Language :: English',
               'Operating System :: OS Independent',
               'Programming Language :: Python',
-              'Programming Language :: Python :: 2.6',
-              'Programming Language :: Python :: 2.7',
               'Topic :: Documentation :: Sphinx',
               'Topic :: Software Development :: Libraries :: Python Modules',
           ],
